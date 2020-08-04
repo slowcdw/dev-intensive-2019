ea12e0d4a4aeecfd1efd7b9478261f140bb50964
SlowCDW <1quick1@list.ru>
2020-08-04 23:55:12 +0300

    4 lesson done_2

--- app/src/androidTest/java/ru/skillbranch/devintensive/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/ru/skillbranch/devintensive/ExampleInstrumentedTest.kt
@@ -1,7 +1,10 @@
 package ru.skillbranch.devintensive
 
-import android.support.test.InstrumentationRegistry
-import android.support.test.runner.AndroidJUnit4
+//import android.support.test.InstrumentationRegistry
+//import android.support.test.runner.AndroidJUnit4
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.platform.app.InstrumentationRegistry
+//import androidx.test.runner.AndroidJUnit4
 
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -18,6 +21,7 @@ class ExampleInstrumentedTest {
     @Test
     fun useAppContext() {
         // Context of the app under test.
+
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
         assertEquals("ru.skillbranch.devintensive", appContext.packageName)
     }
