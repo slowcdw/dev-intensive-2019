933096f54f001584e3a0a57fd33eb1a596305ba3
SlowCDW <1quick1@list.ru>
2020-08-04 22:47:57 +0300

    4 lesson done

--- app/src/main/java/ru/skillbranch/devintensive/extensions/Activity.kt
+++ app/src/main/java/ru/skillbranch/devintensive/extensions/Activity.kt
@@ -3,7 +3,6 @@ package ru.skillbranch.devintensive.extensions
 import android.app.Activity
 import android.view.View
 import android.view.inputmethod.InputMethodManager
-import ru.skillbranch.devintensive.MainActivity
 
 fun Activity.hideKeyboard() {
 
