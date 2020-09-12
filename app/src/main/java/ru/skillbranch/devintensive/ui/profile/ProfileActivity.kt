d4ba62b14a9fa964d494a4b8baf360080befaa8d
SlowCDW <1quick1@list.ru>
2020-09-13 00:01:25 +0300

    4 star_4

--- app/src/main/java/ru/skillbranch/devintensive/ui/profile/ProfileActivity.kt
+++ app/src/main/java/ru/skillbranch/devintensive/ui/profile/ProfileActivity.kt
@@ -15,7 +15,7 @@ import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import kotlinx.android.synthetic.main.activity_profile.*
 import ru.skillbranch.devintensive.R
-import ru.skillbranch.devintensive.extensions.validateUrl
+import ru.skillbranch.devintensive.extensions.validUrl
 import ru.skillbranch.devintensive.models.Profile
 import ru.skillbranch.devintensive.viewmodels.ProfileViewModel
 
@@ -87,7 +87,7 @@ class ProfileActivity : AppCompatActivity() {
 
         et_repository.addTextChangedListener(object: TextWatcher {
             override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-                if (s.toString().validateUrl()) {
+                if (s.toString().validUrl()) {
                     isValidRepo = true
                     wr_repository.error = null
                     wr_repository.isErrorEnabled = false
